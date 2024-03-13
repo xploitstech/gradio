10c1fec5cafd6a86ea9316ec21c45ebd64c197e7
xploits tech <65187222+xploitstech@users.noreply.github.com>
2024-03-13 23:21:14 +0300

    Update +layout.svelte

--- js/_website/src/routes/+layout.svelte
+++ js/_website/src/routes/+layout.svelte
@@ -1,89 +1,69 @@
-<script context="module" lang="ts">
-	declare global {
-		interface Window {
-			__gradio_mode__: "app" | "website";
-			__gradio_space__: string | null;
-		}
-	}
-	import type { media_query as MQ } from "../lib/utils";
-	export let store: ReturnType<typeof MQ>;
-</script>
-
-<script lang="ts">
-	import "$lib/assets/style.css";
-	import "$lib/assets/prism.css";
-
-	import Header from "$lib/components/Header.svelte";
-	import Footer from "$lib/components/Footer.svelte";
-
-	import { media_query } from "../lib/utils";
-	store = media_query();
-
-	import { browser } from "$app/environment";
-	if (browser) {
-		window.__gradio_mode__ = "website";
-	}
-
-	import CopyButton from "$lib/components/CopyButton.svelte";
+<script>
+	import "@gradio/theme";
+	import "../../../app/test/mocks/theme.css";
+	import { page } from "$app/stores";
 	import { afterNavigate } from "$app/navigation";
 
-	afterNavigate(() => {
-		for (const node of document.querySelectorAll(".codeblock")) {
-			let children = Array.from(node.querySelectorAll("pre, a"));
-			let textContent = node.textContent;
-			node.innerHTML = "";
+	const links = [
+		["/embeds", "Embeds"],
+		["/client-browser", "Client-Browser"],
+		["/client-node", "Client-Node"]
+	];
 
-			new CopyButton({
-				target: node,
-				props: {
-					content: textContent ?? ""
-				}
-			});
-			for (const child of children) {
-				node.appendChild(child);
-			}
-		}
-	});
+	$: afterNavigate(() => (location.hash = $page.url.pathname.replace("/", "")));
 </script>
 
 <svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
-		rel="stylesheet"
+		rel="preconnect"
+		href="https://fonts.gstatic.com"
+		crossorigin="anonymous"
 	/>
-	<script
-		type="module"
-		crossorigin="true"
-		src="https://cdn.jsdelivr.net/npm/@gradio/lite/dist/lite.js"
-	></script>
-
-	<script
-		async
-		src="https://www.googletagmanager.com/gtag/js?id=UA-156449732-1"
-	></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag("js", new Date());
-		gtag("config", "UA-156449732-1", {
-			cookie_flags: "samesite=none;secure"
-		});
-	</script>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&amp;display=swap"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&amp;display=swap"
+	/>
 </svelte:head>
 
-<Header />
-
-<slot />
-
-<Footer />
+<div class="prose">
+	<h1><span style="color: var(--color-accent);">Gradio</span> Test Space</h1>
+	<ul>
+		{#each links as [url, name]}
+			<li><a href={url} class:active={url === $page.route.id}>{name}</a></li>
+		{/each}
+	</ul>
+	<slot />
+</div>
 
 <style>
-	:global(
-			body:has(.playground) .main-header,
-			body:has(.playground) .main-footer
-		) {
-		display: none;
+	div {
+		margin: auto;
+		width: 100%;
+		max-width: 800px;
+	}
+
+	ul {
+		display: flex;
+		gap: var(--spacing-xxl);
+		list-style: none;
+	}
+
+	a {
+		color: var(--link-text-color);
+		text-decoration: none;
+	}
+
+	a.active {
+		color: var(--color-accent);
+		font-weight: bold;
+	}
+
+	.prose {
+		margin-top: var(--size-5);
 	}
 </style>
